529b48b9861dff5229958c8d40a33f0c43dd8a93
Sam <sambolton.kw@gmail.com>
2024-03-24 01:25:36 -0400

    added delelete task

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ let nextId = JSON.parse(localStorage.getItem("nextId")) || 0;
 function generateTaskId() {
     return '_' + Math.random().toString(36).slice(2, 9);
     //return nextId++;
-}
+};
 
 // Todo: create a function to create a task card
 function createTaskCard(task) {
@@ -18,13 +18,18 @@ function createTaskCard(task) {
     let $deleteButton = $("<button>").addClass("btn btn-danger delete-task").attr("data-task-id", task.id).text("Delete");
 
     $taskCard.append($cardTitle, $cardDueDate, $cardDescription, $deleteButton);
+    console.log(`append1`)
+//event leister to delete
+    $deleteButton.click(handleDeleteTask);
 
    return $taskCard;
-}
-
+   console.log(`return`)
+};
+let renderCounter = 0; // Counter to track the number of renderings
 // Todo: create a function to render the task list and make cards draggable
 function renderTaskList() {
-    
+    renderCounter++; // Increment the rendering counter
+    console.log(`Render ${renderCounter} started`);
     const laneMap = {
         "Not Yet Started": "#to-do",
         "In Progress": "#in-progress",
@@ -33,14 +38,14 @@ function renderTaskList() {
 console.log(laneMap);
 
     taskList.forEach(task => {
-        console.log(1);
+        console.log("processing task:", task);
         let $taskCard = createTaskCard(task);
-        console.log(2);
+        console.log("created task card:", $taskCard);
         let $targetLane = $(laneMap[task.progressState]);
-        console.log(3);
+        console.log("Target lane:", $targetLane);
     
         $targetLane.find(".card-body").append($taskCard);
-        console.log(4);
+        console.log("Task card appended to lane:", $targetLane);
     
 // making the card draggable.  Added extra features to experiment
         $taskCard.draggable({
@@ -49,15 +54,16 @@ console.log(laneMap);
             //delay: "delay",
             cursor: "crosshair"
           });
-         console.log(6);
+          console.log("Task card made draggable.");
     })
-    console.log(7);
-}
+    console.log(`Render ${renderCounter} completed`);
+};
 
 // Todo: create a function to handle adding a new task
 function handleAddTask(event){
+    console.log("handleAddTask INVOKED")
     event.preventDefault();
-
+console.log(`handle`);
 
     //retrieving values from form
     let title = $("#taskTitle").val();
@@ -73,37 +79,42 @@ function handleAddTask(event){
         description: description,
         progressState: "Not Yet Started"
     };
-
+console.log(`handle new task`)
     taskList.push(newTask);
-
+console.log(`handle push`);
     localStorage.setItem("tasks", JSON.stringify(taskList));
- 
+ console.log(`handle storage`)
     //Clear form
     $("#taskTitle").val("");
     $("#taskDueDate").val("");
     $("#taskDescription").val("");
-
+console.log(`clear form`);
     renderTaskList();
-   console.log(`render`);
+   console.log(` handle render`);
     $("#formModal").modal("hide");
+    console.log(`hide`)
     
-}
+};
 
 // Todo: create a function to handle deleting a task
 function handleDeleteTask(event){
-
-}
+    let taskId = $(this).attr("data-task-id");
+    taskList = taskList.filter(task => task.id !== taskId);
+    localStorage.setItem("tasks", JSON.stringify(taskList));
+    $(this).closest(".card-body").remove();
+};
 
 // Todo: create a function to handle dropping a task into a new status lane
 function handleDrop(event, ui) {
 
-}
+};
+
+
 
 // Todo: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
 $(document).ready(function () {
-    
-    $("#formModal .modal-footer button").click(function(event) {
-       handleAddTask(event);
-       
-});
+    $(document).ready(function () {
+        $("#formModal .modal-footer button").click(handleAddTask);
+        renderTaskList(); // Render task list when the page 
+    });
 });
